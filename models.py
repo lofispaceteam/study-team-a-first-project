10e089ad5a4f2990ddeee1b3fe52142f197822ca
eixone <k.filyakov@yandex.ru>
2025-05-03 10:33:28 +0500

    Исправил true на True в строке 7

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key = True, index = true)
+    id = Column(Integer, primary_key = True, index = True)
     first_name = Column(String, nullable = False)
     last_name = Column(String, nullable = False)
     email = Column(String, unique = True, index = True, nullable = False)
