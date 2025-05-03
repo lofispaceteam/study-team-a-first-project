a83f9468926a32f8eb70766f3fa9d5281f901deb
eixone <k.filyakov@yandex.ru>
2025-05-03 10:31:40 +0500

    прописал создание таблицы

--- models.py
+++ models.py
@@ -4,4 +4,9 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    
+    id = Column(Integer, primary_key = True, index = true)
+    first_name = Column(String, nullable = False)
+    last_name = Column(String, nullable = False)
+    email = Column(String, unique = True, index = True, nullable = False)
+    phone_number = Column(String, unique = True, nullable = False)
+    password_hash = Column(String, nullable = False)
