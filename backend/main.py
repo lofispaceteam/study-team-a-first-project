a9a431461ff9aa8cbe50898e74733df7cf21cd46
eixone <k.filyakov@yandex.ru>
2025-05-06 13:54:18 +0500

    Добавил шифрование пароля

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,9 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)    
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
