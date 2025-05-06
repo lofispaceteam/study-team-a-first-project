bbd0ecdadc680f530534723a202ea34466d02aa2
eixone <k.filyakov@yandex.ru>
2025-05-06 13:45:50 +0500

    Добавил Защищенный маршрут для работы с пользователем

--- backend/main.py
+++ backend/main.py
@@ -30,9 +30,6 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
