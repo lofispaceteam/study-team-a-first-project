29c76467dda21511717b5708a4249c4d2588670f
eixone <k.filyakov@yandex.ru>
2025-05-13 17:18:57 +0500

    Добавил в файл models.py колонну для фотографии пользователя

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,4 @@ class User(Base):
     email = Column(String, unique = True, index = True, nullable = False)
     phone_number = Column(String, unique = True, nullable = False)
     password_hash = Column(String, nullable = False)
+    photo_url = Column(String, nullable = True)
