b644c996d59949f33eea72eb25919b4cf0314d31
eixone <k.filyakov@yandex.ru>
2025-05-18 22:52:22 +0500

    Добавил комментарии в файле models.py

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+# Модель пользователя для таблицы users в базе данных
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users" # Название таблицы в базе данных
 
+    # Уникальный идентификатор пользователя (первичный ключ)
     id = Column(Integer, primary_key = True, index = True)
+    
     first_name = Column(String, nullable = False)
     last_name = Column(String, nullable = False)
     email = Column(String, unique = True, index = True, nullable = False)
