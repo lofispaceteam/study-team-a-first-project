435c01f73868e6e92667af6a007aef783d6dee17
eixone <k.filyakov@yandex.ru>
2025-05-22 16:07:59 +0500

    Добавил класс RefreshToken в models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
 from database import Base
 
 # Модель пользователя для таблицы users в базе данных
@@ -14,3 +16,13 @@ class User(Base):
     phone_number = Column(String, unique = True, nullable = False)
     password_hash = Column(String, nullable = False)
     photo_path = Column(String, nullable = True)
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String, unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="refresh_tokens")
